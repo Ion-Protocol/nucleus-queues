a652c8ea4aa95ad70b9c602cd998e52b4b157eb4
Carson <carsonpcase@gmail.com>
2025-02-05 14:01:25 -0500

    feat: recipient for AtomicQueueUCP

--- test/AtomicQueueUCP.t.sol
+++ test/AtomicQueueUCP.t.sol
@@ -64,7 +64,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         vm.startPrank(USER_ONE);
@@ -75,7 +75,7 @@ contract AtomicQueueUCPTest is Test {
         assertEq(savedRequest.deadline, request.deadline);
         assertEq(savedRequest.atomicPrice, request.atomicPrice);
         assertEq(savedRequest.offerAmount, request.offerAmount);
-        assertEq(savedRequest.inSolve, false);
+        assertEq(savedRequest.recipient, request.recipient);
         vm.stopPrank();
     }
 
@@ -84,7 +84,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         bool isValid = queue.isAtomicRequestValid(offerToken, USER_ONE, request);
@@ -94,7 +94,7 @@ contract AtomicQueueUCPTest is Test {
     function test_IsAtomicRequestValid_ExpiredDeadline() public {
         uint64 deadline = uint64(block.timestamp + 1 hours);
         AtomicQueueUCP.AtomicRequest memory request =
-            AtomicQueueUCP.AtomicRequest({ deadline: deadline, atomicPrice: 5e6, offerAmount: 1e18, inSolve: false });
+            AtomicQueueUCP.AtomicRequest({ deadline: deadline, atomicPrice: 5e6, offerAmount: 1e18, recipient: USER_ONE });
 
         // Move time forward past the deadline
         vm.warp(deadline + 1);
@@ -108,7 +108,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1000e18, // More than user has
-            inSolve: false
+            recipient: USER_ONE
         });
 
         bool isValid = queue.isAtomicRequestValid(offerToken, USER_ONE, request);
@@ -120,7 +120,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         vm.startPrank(USER_ONE);
@@ -153,14 +153,14 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         AtomicQueueUCP.AtomicRequest memory request2 = AtomicQueueUCP.AtomicRequest({
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 4e6,
             offerAmount: 2e18,
-            inSolve: false
+            recipient: USER_TWO
         });
 
         vm.prank(USER_ONE);
@@ -189,7 +189,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         vm.prank(USER_ONE);
@@ -210,7 +210,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         vm.prank(USER_ONE);
@@ -237,7 +237,7 @@ contract AtomicQueueUCPTest is Test {
             deadline: uint64(block.timestamp + 1 hours),
             atomicPrice: 5e6,
             offerAmount: 1e18,
-            inSolve: false
+            recipient: USER_ONE
         });
 
         vm.prank(USER_ONE);
@@ -260,7 +260,7 @@ contract AtomicQueueUCPTest is Test {
     function test_ViewSolveMetaData_MultipleFlags() public {
         uint64 deadline = uint64(block.timestamp + 1 hours);
         AtomicQueueUCP.AtomicRequest memory request =
-            AtomicQueueUCP.AtomicRequest({ deadline: deadline, atomicPrice: 5e6, offerAmount: 1e18, inSolve: false });
+            AtomicQueueUCP.AtomicRequest({ deadline: deadline, atomicPrice: 5e6, offerAmount: 1e18, recipient: USER_ONE });
 
         vm.prank(USER_ONE);
         queue.updateAtomicRequest(offerToken, wantToken, request);
